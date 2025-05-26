6b7fd9942d6f2e0031f2c93839ba31a80b90b555
Sohit Vyas <sohitvyas@Sohits-MacBook-Air.local>
2025-05-26 14:17:31 +0530

    add logout button where no templete found and remove extra comments from home screen

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -15,39 +15,11 @@ class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
   final homeController = Get.put(HomeController());
 
-  //   body: Obx(() {
-  //   return !homeController.socketService.isSocketConnected.value
-  //       ? const NoIniternetScreen()
-  //       : FutureBuilder(
-  //           future: homeController.deviceTempData(),
-  //           builder:
-  //               (context, AsyncSnapshot<DeviceTempleteDataModel?> snap) {
-  //             return snap.data == null
-  //                 ? const Center(child: CircularProgressIndicator())
-  //                 : SafeArea(
-  //                     top: true,
-  //                     child: snap.data!.data!.device!.templateId ==
-  //                             "ed1f51de-2d13-4bf7-bb9d-3c42ef93f0f0"
-  //                         ? MedicalTemp2(
-  //                             tempData: homeController.templateData,
-  //                           )
-  //                         : const NoTempleteFound());
-  //           });
-  // }),
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Obx(
-            () => homeFunction(homeController.socketService.refreshKey.value))
-        // body: homeFunction(),
-        // body: Obx(() {
-        //   // if (!homeController.socketService.isSocketConnected.value) {
-        //   //   return const Center(child: CircularProgressIndicator());
-        //   // }
-        //   return homeFunction();
-        // }),
-        );
+            () => homeFunction(homeController.socketService.refreshKey.value)));
   }
 
   Widget homeFunction(int refreshKey) {
@@ -78,89 +50,10 @@ class HomeScreen extends StatelessWidget {
           } else if (templateId == "c9247836-2243-4f40-acfb-be5d2dc15a9d") {
             return RetailerTemplate(tempData: homeController.templateData);
           } else {
-            return const NoTemplateFound();
+            return NoTemplateFound();
           }
         }
       },
     );
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Obx(() {
-  //     return Scaffold(
-  //       body: !homeController.socketService.isSocketConnected.value
-  //           ? const NoTemplateFound()
-  //           : FutureBuilder(
-  //               future: homeController.deviceTempData(),
-  //               builder:
-  //                   (context, AsyncSnapshot<DeviceTempleteDataModel?> snap) {
-  //                 if (snap.connectionState == ConnectionState.waiting) {
-  //                   return const Center(child: CircularProgressIndicator());
-  //                 } else if (snap.hasError && snap.data == null) {
-  //                   return const NoInternetScreen();
-  //                 } else if (snap.data == null) {
-  //                   return Center(
-  //                     child: Text(
-  //                       "Something went wrong.\nPlease check your internet or contact tech team.",
-  //                       textAlign: TextAlign.center,
-  //                       style: TextStyle(
-  //                           fontWeight: FontWeight.bold, fontSize: 50.sp),
-  //                     ),
-  //                   );
-  //                 } else {
-  //                   final templateId = snap.data!.data!.device!.templateId;
-
-  //                   if (templateId == "ed1f51de-2d13-4bf7-bb9d-3c42ef93f0f0") {
-  //                     return MedicalTemp2(
-  //                         tempData: homeController.templateData);
-  //                   } else if (templateId ==
-  //                       "c9247836-2243-4f40-acfb-be5d2dc15a9d") {
-  //                     return RetailerTemplate(
-  //                         tempData: homeController.templateData);
-  //                   } else {
-  //                     return const NoTemplateFound();
-  //                   }
-  //                 }
-  //               },
-  //             ),
-  //     );
-  //   });
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   if (homeController.loading.value) {
-  //     return const Scaffold(
-  //       body: Center(child: CircularProgressIndicator()),
-  //     );
-  //   }
-
-  //   if (homeController.hasError.value ||
-  //       homeController.deviceTemplateData.value == null) {
-  //     return const Scaffold(
-  //       body: NoInternetScreen(),
-  //     );
-  //   }
-
-  //   final templateId =
-  //       homeController.deviceTemplateData.value!.data?.device?.templateId;
-
-  //   return Scaffold(
-  //     body: Builder(
-  //       builder: (context) {
-  //         switch (templateId) {
-  //           case "ed1f51de-2d13-4bf7-bb9d-3c42ef93f0f0":
-  //             return MedicalTemp2(tempData: homeController.templateData);
-
-  //           case "c9247836-2243-4f40-acfb-be5d2dc15a9d":
-  //             return RetailerTemplate(tempData: homeController.templateData);
-
-  //           default:
-  //             return const NoTemplateFound();
-  //         }
-  //       },
-  //     ),
-  //   );
-  // }
 }
--- lib/screen/home/templates/no_template_found.dart
+++ lib/screen/home/templates/no_template_found.dart
@@ -1,25 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import '../controller/home_controller.dart';
 
 class NoTemplateFound extends StatelessWidget {
-  const NoTemplateFound({super.key});
-
+  NoTemplateFound({super.key});
+  final homeController = Get.find<HomeController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "No template found contact tech team",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50.sp),
-            )
-          ],
-        ),
+      body: Stack(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "No template found contact tech team",
+                  textAlign: TextAlign.center,
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 50.sp),
+                )
+              ],
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.transparent,
+              shadowColor: Colors.transparent,
+              surfaceTintColor: Colors.transparent,
+              padding: EdgeInsets.zero,
+            ),
+            child: const SizedBox(),
+            onPressed: () async {
+              await homeController.logOut();
+            },
+          ),
+        ],
       ),
     );
   }
